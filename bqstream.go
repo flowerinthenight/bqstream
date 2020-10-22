8cdc6c2e89339a1681ed128fe121056211a41126
flowerinthenight <flowerinthenight@pm.me>
2020-10-22 11:11:34 +0900

    go mod

--- bqstream.go
+++ bqstream.go
@@ -1,1 +1,133 @@
 package bqstream
+
+import (
+	"context"
+	"fmt"
+	"runtime"
+
+	"cloud.google.com/go/bigquery"
+	"github.com/golang/glog"
+)
+
+type Option interface {
+	Apply(*BqStream)
+}
+
+type withProjectId string
+
+func (w withProjectId) Apply(o *BqStream) { o.projectId = string(w) }
+
+func WithProjectId(v string) Option { return withProjectId(v) }
+
+type withRegion string
+
+func (w withRegion) Apply(o *BqStream) { o.region = string(w) }
+
+func WithRegion(v string) Option { return withRegion(v) }
+
+type withDataset string
+
+func (w withDataset) Apply(o *BqStream) { o.dataset = string(w) }
+
+func WithDataset(v string) Option { return withDataset(v) }
+
+type withTable string
+
+func (w withTable) Apply(o *BqStream) { o.table = string(w) }
+
+func WithTable(v string) Option { return withTable(v) }
+
+type BqStream struct {
+	projectId string
+	region    string
+	dataset   string
+	table     string
+	client    *bigquery.Client
+	inserter  *bigquery.Inserter
+	started   bool
+	wq        chan interface{}
+	wqdone    chan struct{}
+	wqcnt     int
+}
+
+func (v *BqStream) worker(id int) {
+	defer func() {
+		glog.Infof("worker %v stopped", id)
+		v.wqdone <- struct{}{}
+	}()
+
+	for j := range v.wq {
+		err := v.inserter.Put(context.Background(), j)
+		if err != nil {
+			glog.Errorf("bqstream.Put failed: %v", err)
+		}
+	}
+}
+
+// Add puts records to the streamer. It expects a slice of structs that implements
+// the bigquery.ValueSaver interface.
+func (v *BqStream) Add(txns interface{}) {
+	if !v.started {
+		return
+	}
+
+	v.wq <- txns
+}
+
+type StartInput struct {
+	StreamCount int
+}
+
+func (v *BqStream) Start(ctx context.Context, in ...StartInput) error {
+	var err error
+	v.client, err = bigquery.NewClient(ctx, v.projectId)
+	if err != nil {
+		return fmt.Errorf("bigquery.NewClient failed: %w", err)
+	}
+
+	v.inserter = v.client.Dataset(v.dataset).Table(v.table).Inserter()
+
+	if len(in) > 0 {
+		if in[0].StreamCount > 0 {
+			v.wqcnt = in[0].StreamCount
+		}
+	}
+
+	glog.Infof("starting %v worker(s) for bqstream", v.wqcnt)
+	v.wq = make(chan interface{}, v.wqcnt)
+	v.wqdone = make(chan struct{}, v.wqcnt)
+	for i := 0; i < v.wqcnt; i++ {
+		go v.worker(i)
+	}
+
+	v.started = true
+	return nil
+}
+
+func (v *BqStream) Close() {
+	if !v.started {
+		return
+	}
+
+	v.client.Close()
+	close(v.wq)
+	for i := 0; i < v.wqcnt; i++ {
+		<-v.wqdone
+	}
+}
+
+func New(opts ...Option) *BqStream {
+	v := &BqStream{
+		projectId: "mobingi-main",
+		region:    "asia-northeast1",
+		dataset:   "authd_txn",
+		table:     "prod",
+		wqcnt:     runtime.NumCPU(),
+	}
+
+	for _, opt := range opts {
+		opt.Apply(v)
+	}
+
+	return v
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
 module github.com/flowerinthenight/bqstream
 
 go 1.15
+
+require (
+	cloud.google.com/go v0.70.0 // indirect
+	cloud.google.com/go/bigquery v1.12.0
+	cloud.google.com/go/storage v1.12.0 // indirect
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
+	github.com/google/martian/v3 v3.1.0 // indirect
+	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
+	golang.org/x/sys v0.0.0-20201020230747-6e5568b54d1a // indirect
+	golang.org/x/tools v0.0.0-20201021214918-23787c007979 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20201021134325-0d71844de594 // indirect
+	google.golang.org/grpc v1.33.1 // indirect
+)
